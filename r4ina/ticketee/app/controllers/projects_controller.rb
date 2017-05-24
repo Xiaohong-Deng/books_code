b02e6d90e5d32f8510d8793c08a931b5d2619ce9
nicenick <worldsky86rough@gmail.com>
2017-05-24 16:24:13 +0800

    Add validations to ensure names are specified when creating projects

--- r4ina/ticketee/app/controllers/projects_controller.rb
+++ r4ina/ticketee/app/controllers/projects_controller.rb
@@ -16,6 +16,11 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
+      # render is not considered as another request
+      # because it doesn't require you to go through
+      # an action again
+      flash.now[:alert] = "Project has not been created."
+      render 'new'
     end
   end
 
