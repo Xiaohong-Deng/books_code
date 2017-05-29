d75708eefc6149fb56b07f72333d7a3875a89d57
nicenick <worldsky86rough@gmail.com>
2017-05-29 23:01:49 +0800

    Handle authorization errors gracefully

--- r4ina/ticketee/spec/controllers/projects_controller_spec.rb
+++ r4ina/ticketee/spec/controllers/projects_controller_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe ProjectsController, type: :controller do
     message = "The project you were looking for could not be found."
     expect(flash[:alert]).to eq message
   end
+
+  it 'handles permission errors by redirecting' do
+    # the point of stubbing out current_user is
+    # we don't log in and if anything goes wrong with
+    # log in feature we hope it doesn't affect our testing
+    # this functionality
+    allow(controller).to receive(:current_user)
+
+    project = FactoryGirl.create(:project)
+    # current_user returns nil, show? returns false
+    get :show, id: project
+
+    expect(response).to redirect_to(root_path)
+    message = "You aren't allowed to do that."
+    expect(flash[:alert]).to eq message
+  end
 end
