bc1c5f5c72e8400b3967883d4e59d5870d6ef787
nicenick <worldsky86rough@gmail.com>
2017-06-01 05:05:32 +0800

    Add the ability to attach a file to a ticket

--- r4ina/ticketee/app/models/ticket.rb
+++ r4ina/ticketee/app/models/ticket.rb
@@ -3,4 +3,8 @@ class Ticket < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+  mount_uploader :attachment, AttachmentUploader
+  # define a setter named attachment=, :attachment is a column of
+  # Ticket. AttachmentUploader is a custom Uploader class. You mount
+  # the uploader on the column. It's a subclass of CarrierWave::Uploader
 end
