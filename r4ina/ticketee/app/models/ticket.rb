4f9cd9aefdd68fcd555fe5337b34c8c487173525
nicenick <worldsky86rough@gmail.com>
2017-06-23 02:23:46 +0800

    User now can attach 3 file at a time when creating a ticket

--- r4ina/ticketee/app/models/ticket.rb
+++ r4ina/ticketee/app/models/ticket.rb
@@ -1,10 +1,12 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :author, class_name: "User"
+  has_many :attachments, dependent: :destroy
+  # This little helper tells your model to accept attachment attributes along with ticket
+  # attributes whenever you call methods like new, build, and update. It will also change
+  # how fields_for performs in your form, making it reference the attachments association,
+  # and calling the parameters that get generated in the form attachments_attributes rather than attachments.
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
-  mount_uploader :attachment, AttachmentUploader
-  # define a setter named attachment=, :attachment is a column of
-  # Ticket. AttachmentUploader is a custom Uploader class. You mount
-  # the uploader on the column. It's a subclass of CarrierWave::Uploader
 end
--- r4ina/ticketee/config/routes.rb
+++ r4ina/ticketee/config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'projects#index'
 
+  # nested resources
   resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets
   end
