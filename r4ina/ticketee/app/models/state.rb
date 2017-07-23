3a9c15689c5fc5734aac2da8b229c0690fe092fa
nicenick <worldsky86rough@gmail.com>
2017-07-23 12:49:35 +0800

    Auto-assigned the default state to newly-created tickets

--- r4ina/ticketee/app/models/state.rb
+++ r4ina/ticketee/app/models/state.rb
@@ -1,4 +1,10 @@
 class State < ActiveRecord::Base
+  # should I set a class variable @@default and update it
+  # each time make_default is called to get a better performance?
+  def self.default
+    find_by(default: true)
+  end
+
   def to_s
     name
   end
