3e7ff8df004ab13f2d1ece46c467443f7e1a917f
nicenick <worldsky86rough@gmail.com>
2017-08-05 20:00:25 +0800

    Add basic email ticket notifications

--- r4ina/ticketee/app/models/ticket.rb
+++ r4ina/ticketee/app/models/ticket.rb
@@ -4,7 +4,9 @@ class Ticket < ActiveRecord::Base
   belongs_to :state
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_and_belongs_to_many :tags, uniq: true
+  has_and_belongs_to_many :tags, uniq: true # uniq means retrieve unique tags even if there are dups in db
+  has_and_belongs_to_many :watchers, join_table: "ticket_watchers",
+    class_name: "User", uniq: true
   attr_accessor :tag_names
   # This little helper tells your model to accept attachment attributes along with ticket
   # attributes whenever you call methods like new, build, and update. It will also change
@@ -15,6 +17,7 @@ class Ticket < ActiveRecord::Base
   validates :description, presence: true, length: { minimum: 10 }
 
   before_create :assign_default_state
+  after_create :author_watches_me
 
   searcher do
     label :tag, from: :tags, field: "name" # label is what you use to search in the search bar
@@ -37,5 +40,11 @@ class Ticket < ActiveRecord::Base
   private
     def assign_default_state
       self.state ||= State.default
+    end
+
+    def author_watches_me
+      if author.present? && ! self.watchers.include?(author)
+        self.watchers << author
+      end
     end
 end
